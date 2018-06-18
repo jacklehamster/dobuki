760b0da63670f3861796674f7fcbaa968ec1ce09
Jack Le Hamster <jacklehamster@gmail.com>
2018-06-18 16:40:17 -0700

    Update

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,14 +1,11 @@
-import express from "express"
+import express from "express";
 
-
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({hello: 'world'});
+    res.json({ hello: 'world' });
 });
-
-
-var port = process.env.PORT || 3000;
 
 var server = app.listen(port, () => {
     console.log('Service started on port :' + port);
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({hello: 'world'});
+});
+
+
+var server = app.listen(port, () => {
+    console.log('Service started on port :' + port);
+});
