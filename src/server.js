97f825362b835c2f35a1bec5438eeded45af9635
Jack Le Hamster <jacklehamster@gmail.com>
2018-06-19 01:25:07 -0700

    Test again

--- src/server.js
+++ src/server.js
@@ -5,10 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    const test = <div></div>;
     res.json({hello: 'world'});
 });
 
 
-var server = app.listen(port, () => {
+app.listen(port, () => {
     console.log('Service started on port :' + port);
 });
